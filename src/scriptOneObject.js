6a5d38b9b96fb97b59cff4303b16e98d350645e7
Ruxin Zheng <RuxinZ@users.noreply.github.com>
2024-08-08 08:22:19 -0700

    transform objects, group objects

--- src/scriptOneObject.js
+++ src/scriptOneObject.js
@@ -6,30 +6,25 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
-// Objects
-const group = new THREE.Group();
-scene.add(group);
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: '#00f58e' })
-);
+// Position
+mesh.position.set(2, 0, 1);
+// console.log(mesh.position.length());
 
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: '#ffc700' })
-);
+// Scale
+// mesh.scale.x = 2;
+mesh.scale.set(3, 0.5, 1);
 
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: '#ff001f' })
-);
-
-cube1.position.set(-1, 0, 1);
-cube2.position.set(-1, 1, 1);
-
-group.add(cube1, cube2, cube3);
-group.scale.x = 2;
+// Rotation
+// mesh.rotateY = 60; // degree
+mesh.rotation.reorder('YXZ'); // need to set it first
+mesh.rotation.y = Math.PI * 0.5;
+mesh.rotation.x = Math.PI * 0.5;
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper();
@@ -46,6 +41,10 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 5;
 camera.position.x = 1;
 scene.add(camera);
+
+camera.lookAt(mesh.position);
+
+// console.log(mesh.position.distanceTo(camera.position));
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
