13534ac062cb3b7b38b555f95d0fe4b4f452bdb0
Ruxin Zheng <RuxinZ@users.noreply.github.com>
2024-08-08 07:36:10 -0700

    move source files into src folder

--- src/script.js
+++ src/script.js
@@ -11,6 +11,9 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 
+mesh.position.set(2, -1, -1);
+console.log(mesh.position.length());
+
 scene.add(mesh);
 
 // Sizes
